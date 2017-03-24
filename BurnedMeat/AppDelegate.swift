b58ec1f0e56468d040b7b13e617a8c5cc53774a3
Alexander von Below <Alex@vonBelow.Com>
2017-03-24 17:54:31 +0100

    First Version

---
+++ BurnedMeat/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  BurnedMeat
+//
+//  Created by Alexander v. Below on 24.03.17.
+//  Copyright © 2017 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
